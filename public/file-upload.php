9dfd6b1eb9cf60c7fc73175f1c4ebb41081cf28b
Joshua <jo4christ48@yahoo.com>
2022-09-14 12:04:42 +0100

    added read uploaded file

---
+++ public/file-upload.php
@@ -0,0 +1,89 @@
+<?php
+
+function formatNum(string $num){
+    if(str_starts_with($num, '234') && strlen($num) == 13){
+        return '+' . $num;
+    }
+
+    if(str_starts_with($num, '0') && strlen($num) == 11){
+        return '+234' . substr($num, 1);
+    }
+    return $num;
+}
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    $contacts = [];
+
+    if($method == 'POST'){
+        $uploaded = $_FILES['contacts'];
+
+        $file = fopen($uploaded['tmp_name'], 'r');
+            while(!feof($file)){
+
+                $line = fgets($file);
+
+                if(!empty(trim($line))){
+                    $data = explode('|', $line);
+                    // echo formatNum($data[1]);
+
+                    // format numbers to +234
+                    array_push($contacts,  [$data[0], formatNum(trim($data[1]))]);
+                }
+            }
+        fclose($file);
+    }
+
+    // print_r($contacts);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+</head>
+<body>
+    <div>
+        <form action="#" method="POST" enctype="multipart/form-data">
+            <div>
+                <label for="contacts">Upload Contacts</label>
+                <br>
+                <input type="file" name="contacts" id="contacts" required >
+            </div>
+
+            <div>
+                <button type="submit">Upload</button>
+            </div>
+        </form>
+    </div>
+
+    <div>
+        <?php if(count($contacts) > 0){?>
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">SN</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone Number</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($contacts as $key => $contact){?>
+                        <tr>
+                            <th scope="row"><?=$key + 1?></th>
+                            <td><?=$contact[0]?></td>
+                            <td><?=$contact[1]?></td>
+                        </tr>
+                    <?php }?>
+                </tbody>
+            </table>
+        <?php }else {?>
+            <div>The file does not contain any contact.</div>
+        <?php } ?>
+    </div>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,14 +1,68 @@
 <?php 
-if(count([]) > 0){
+
+// function extractFile($file)
+// {
+//     while(!feof($file)){
+//         $line = fgets($file);
     
+//         // checks if row is not empty
+//         if(!empty(trim($line))){
+//            yield $line;
+//         }
+//     }
+
+// fclose($file)
+// }
+// if(count([]) > 0){
+    
+// }
+// $file = file_get_contents('https://kc.kobotoolbox.org/media/original?media_file=pwfoyo%2Fattachments%2F547b37bfadc140c188add585d0cdedf1%2F113bd6d0-0789-4bf2-b75f-adf93c131eb9%2F1662474743369.jpg');
+
+// echo $file;
+
+$file = fopen(__DIR__ . '/data/users.txt', 'r');
+
+// $contents = iterator_to_array(extractFile($file));
+
+// print_r($contents);
+
+while(!feof($file)){
+    $line = fgets($file);
+
+    // checks if row is not empty
+    if(!empty(trim($line))){
+        echo $line;
+    }
+    // echo ftell($file) . "\n";
+    // fseek($file, ftell($file));
+
+    if(ftell($file) >= 15){
+        break;
+    }
+
+    // if(feof($file)){
+    //     rewind($file);
+    // }
 }
-// $file = file_get_contents(__DIR__ . '/users.txt');
-$handle = fopen(__DIR__ . "/data/users.txt", 'w+');
+// $size = filesize(__DIR__ . '/data/users.txt');
 
-$content = "James Bond\nVictor\n";
+// echo $size . "\n";
 
-fwrite($handle, $content);
-fclose($handle);
+// $read = fread($file, $size);
+// $data = explode("\n", $read);
+
+// foreach($data as $user){
+//     echo str_replace("|", "-", $user) . "\n";
+// }
+// echo $read;
+
+
+// $handle = fopen(__DIR__ . "/data/users.txt", 'w+');
+
+// $content = "James Bond\nVictor\n";
+
+// fwrite($handle, $content);
+// fclose($handle);
 
 // $users = @fopen(__DIR__ . "/customer.txt", 'r');
 
